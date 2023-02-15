cd360a2f9b00099e141daefadf6b453789d7297c
deepakkumar <dk44861@gmail.com>
2023-02-15 15:49:44 +0530

    corrected mistakes

--- Frontend/src/Components/Mainpage/Info/Table.jsx
+++ Frontend/src/Components/Mainpage/Info/Table.jsx
@@ -7,13 +7,14 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-export default function BasicTable({data}) {
+export default function BasicTable({data ,url}) {
   return (
     <TableContainer component={Paper}>
-        <h5>Url:   </h5>
+        <h5>Url:  {url}   </h5>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
+            <TableCell>S.no</TableCell>
             <TableCell>Date</TableCell>
             <TableCell align="right">Info</TableCell>  
           </TableRow>
@@ -25,7 +26,10 @@ export default function BasicTable({data}) {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.date}
+                {i+1}
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {new Date(row.date).toLocaleString()}
               </TableCell>
               <TableCell align="right">{row.info}</TableCell>
             </TableRow>
--- Frontend/src/Components/Mainpage/Info/info.jsx
+++ Frontend/src/Components/Mainpage/Info/info.jsx
@@ -1,24 +1,31 @@
+import { Input } from '@mui/material';
 import React, { useEffect, useState } from 'react'
-import { getData } from '../axios'
+import { getData, toastFuncDanger } from '../axios'
 import BasicTable from './Table';
 
 const Info = () => {
     const [ state , setState] = useState({flag:false ,info:[] });
-    useEffect(() => {
-      getData('pizza')
+    const [url, setUrl] = useState("");
+    
+    const handleSubmit = ()=>{
+      getData(url)
       .then((data)=>{
         console.log(data)
         setState({flag: true ,info:data.data })
       })
-      .catch((err)=>console.log(err))
-    }, [])
-    
+      .catch((err)=>{
+        if(err.response.status === 404){
+           toastFuncDanger("Url Not Found")
+        }
+      })
+    }
   return (
     <div>
         <h1>Get User Url Info</h1>
-        <label>Short Url</label>
-        <input type='text'></input>
-        <BasicTable  data={state.info}/>
+        <label>Short Url &nbsp;</label>
+        <Input type='text' onChange={(e)=>setUrl(e.target.value)}></Input>
+        <button  onClick={()=>handleSubmit()}>Get Info</button>
+        {state.flag && <BasicTable  data={state.info} url={url}/>}
     </div>
   )
 }
